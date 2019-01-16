0925df65874046bb49edeb884a009ad58f84b073
Michel Vocks <michelvocks@gmail.com>
2019-01-16 20:22:36 +0100

    Improved test

--- lib/gaia.rb
+++ lib/gaia.rb
@@ -1,11 +1,36 @@
 require 'rubysdk'
 
 class Main
+    Job1_handler = lambda do |args|
+        args.each do |arg|
+            STDERR.puts "Key: #{arg.key}; Value: #{arg.value}"
+        end
+    end
+    Job2_handler = lambda do |args|
+        STDERR.puts "Job2 has been executed!"
+    end
+    Job3_handler = lambda do |args|
+        STDERR.puts "Job3 has been executed!"
+    end
+
     def self.main
-        job1 = RubySDK::Interface::Job.new(title: "job1", handler: lambda { |args| puts "Job1 has been started" })
-        job2 = RubySDK::Interface::Job.new(title: "job2", handler: lambda { |args| puts "Job2 has been started" })
+        args = []
+        args.push Interface::Argument.new(desc: "Schema name for new database:",
+                                          type: Interface::TextFieldInput,
+                                          key: "username")
+        args.push Interface::Argument.new(type: Interface::VaultInput,
+                                          key: "dbpassword")
+        job1 = Interface::Job.new(title: "job1", 
+                                  handler: Job1_handler,
+                                  args: args)
+        job2 = Interface::Job.new(title: "job2", 
+                                  handler: Job2_handler,
+                                  dependson: ["job1"])
+        job3 = Interface::Job.new(title: "job3",
+                                  handler: Job3_handler,
+                                  dependson: ["job2"])
         begin
-            RubySDK::Serve([job1, job2])
+            RubySDK.Serve([job1, job2, job3])
         rescue => e
             puts "Error occured: #{e}"
         end
