27cc709d313dc86d9f3c1cd070ab1452ee9e92d6
Michel Vocks <michelvocks@gmail.com>
2019-01-17 18:12:39 +0100

    Added example for vault parameter

--- lib/gaia.rb
+++ lib/gaia.rb
@@ -10,14 +10,11 @@ class Main
 
     def self.main
         args = []
-        args.push Interface::Argument.new(desc: "Username for the database schema:",
-                                          # textfield displays a text field in the UI.
+        args.push Interface::Argument.new(key: "dbpassword",
+                                          # VaultInput automatically gets a secret from Gaia's vault.
                                           # You can also use "TextAreaInput", "BooleanInput",
                                           # or "VaultInput".
-                                          type: Interface::TextFieldInput,
-                                          key: "username")
-        args.push Interface::Argument.new(type: Interface::TextAreaInput,
-                                          key: "usernamedesc")
+                                          type: Interface::VaultInput)
         printparam = Interface::Job.new(title: "Print Parameters",
                                         handler: PrintParam,
                                         desc: "This job prints out all given params.",
